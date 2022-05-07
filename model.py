2e03ac6c2a2353c107eb002ff046fc9d5b710f73
RuiSuuun <58921284+RuiSuuun@users.noreply.github.com>
2022-05-07 15:09:17 -0700

    Complete model for CNN

--- model.py
+++ model.py
@@ -8,6 +8,9 @@ class Model(nn.Module):
         self.conv1 = nn.Conv1d(in_channels=in_channels,out_channels=16,kernel_size=(8),padding="same")
         self.actv1 = nn.ReLU()
         self.pool = nn.MaxPool1d(kernel_size = 1, stride = 1)
+        self.linear = nn.Linear(in_features=16, out_features=8)
+        self.actv2 = nn.Sigmoid()
+        self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, x):
         print(x.shape)
@@ -20,4 +23,11 @@ class Model(nn.Module):
         x = self.pool(x)
         print(x.shape)
         x = torch.reshape(x, (-1, x.shape[1] * x.shape[2]))
+        print(x.shape)
+        x = self.linear(x)
+        print(x.shape)
+        x = self.actv2(x)
+        print(x.shape)
+        x = self.dropout(x)
+        print(x.shape)
         return x
